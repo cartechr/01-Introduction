8b27f0312f0d074e15d0c4a5c28154c3685b62f2
Carter <cartechr@iu.edu>
2019-08-28 20:00:30 -0400

    I finished my work

--- main.py
+++ main.py
@@ -4,5 +4,12 @@ import utils
 
 utils.check_version((3,7))
 utils.clear()
+print("Hello, my name is Christian Carter")
 
-print('Hello, Class!')
+print('My name is Christian Carter')
+print('My favorite game is called Call of Duty Blacks Ops 4')
+print('My concerns about this class is the fear of falling behind')
+print('I am excited this whole class in general. Getting to learn how to make games')
+print('My name stack over flow user id is 11991691')
+print('The url to my github profile is https://github.com/cartechr')
+print("Enjoy: https://www.youtube.com/watch?v=dQw4w9WgXcQ")
